69cad94e24c0a14c1a416f74eaf63536fdcae15c
liaojack8 <liaojack8@icloud.com>
2020-09-15 15:12:32 +0800

    Set apktool_2.4.1.jar to be default apktool.

--- bin/apktool/apktool.bat
+++ bin/apktool/apktool.bat
@@ -1,4 +1,4 @@
 @echo off
 if "%PATH_BASE%" == "" set PATH_BASE=%PATH%
 set PATH=%CD%;%PATH_BASE%;
-java -jar "%~dp0\apktool\ShakaApktool.jar" %1 %2 %3 %4 %5 %6 %7 %8 %9
+java -jar "%~dp0\apktool\apktool_2.4.1.jar" %1 %2 %3 %4 %5 %6 %7 %8 %9
